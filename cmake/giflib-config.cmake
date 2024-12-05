30ac60d1232dedf741e73c27c2506a0cb2863a79
Fantasy <181298727@qq.com>
2024-12-05 12:05:36 +0800

    notify

--- cmake/giflib-config.cmake
+++ cmake/giflib-config.cmake
@@ -7,4 +7,13 @@ if(TARGET giflib::giflib)
     get_target_property(GIF_INCLUDE_DIRS giflib::giflib INTERFACE_INCLUDE_DIRECTORIES)
     set(GIF_LIBRARIES giflib::giflib)
 
+    if(NOT TARGET GIF::GIF)
+        add_library(GIF::GIF INTERFACE IMPORTED)
+
+        set_target_properties(GIF::GIF PROPERTIES
+        INTERFACE_LINK_LIBRARIES giflib::giflib
+        )
+    endif()
+
 endif()
+
